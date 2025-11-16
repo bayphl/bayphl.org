0b5626d28a6195dd1b9b4f36a31beabf3682d4ad
Evan Broder <evan@ebroder.net>
2025-11-16 14:32:26 -0600

    Tailwind-ify root

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import netlify from "@astrojs/netlify";
+
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://bayphl.org",
   output: "static",
   adapter: netlify(),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
